e2b30ff92de2202ecaf91f65e9ed96efadbc1d22
tranlo <tranlong.kma@gmail.com>
2019-12-23 10:36:58 +0700

    login, search screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_login/screens/authenticate/SignIn.dart';
+import 'package:flutter_login/screens/home/searchview.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: SignIn(),
+      home: SearchView(),
       );
   }
 }
--- lib/screens/home/searchview.dart
+++ lib/screens/home/searchview.dart
@@ -3,6 +3,48 @@ import 'package:flutter/material.dart';
 class SearchView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+          ),
+          centerTitle: true,
+          title: Text('City'),
+        ),
+        body: Container(
+          padding: EdgeInsets.all(10),
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: Column(
+                  children: <Widget>[
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Enter City Name',
+                      ),
+                      validator: (String value) {
+                        return value == null ? 'Please, Enter City Name' : null;
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  IconButton(
+                    padding: EdgeInsets.only(top: 20),
+                    iconSize: 30,
+                    icon: Icon(Icons.search),
+                    onPressed: () => {},
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
